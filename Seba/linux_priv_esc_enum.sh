a078e1b0835693558abafe57e932ca7e0609d6cf
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2015-01-01 19:18:33 -0500

    messo user enumetaion
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- Seba/linux_priv_esc_enum.sh
+++ Seba/linux_priv_esc_enum.sh
@@ -18,23 +18,47 @@ echo_title(){
 #$1 = short description of the command
 #$2 = comand itself
 echo_command(){
-    echo -e "\e[1;33m$1: \e[\033[1;0m" $($2)
+    echo -e "\e[1;33m$1: \n\e[\033[1;0m"
+    $2
     echo -e "\n"
 }
 
+#function that cicle for each command in the given array and execute the commands
+#
+#$1 = array of title => commands
 run_section_commands(){
-    declare -A argAry1=("${!1}")
-    for K in "${!argAry1[@]}"; do
-        echo $K --- ${argAry1[$K]};
+    #create a new array from the arguent
+    eval "declare -A arg_array="${1#*=}
+    #execute comand
+    for key in "${!arg_array[@]}"; do
+       echo_command $key "${arg_array[$key]}"
     done
 }
 
+#enumerate the system info
 declare -A SYSTEM_INFO=(
-    ["ALL"]="umane -a"
+    ["ALL"]="uname -a"
     ["KERNEL_RELEASE"]="uname -r"
     ["HOSTNAME"]="hostname"
     ["ARCH"]="uname -m"
-    )
+    ["KERNEL_INFO"]="cat /proc/version"
+    ["DISTRO_INFO"]="cat /etc/issue"
+)
+
+#enumerate the info about usera and groups on the system
+declare -A USER_GROUP=(
+    ["ALL_USERS"]="cat /etc/passwd"
+    ["ALL_GROUPS"]="cat /etc/group"
+    ["SUDOERS"]="grep -E ":0:" /etc/passwd"
+    ["USER_CURRENT_LOGGED"]="w"
+)
+
+#enumerate the info about usera and groups on the system
+declare -A CURRENT_USER=(
+    ["WHOAMI"]="whoami"
+    ["ID"]="id"
+)
+
 
 echo -e "\n\e[00;31m#########################################################\e[00m"
 echo -e "\e[00;31m#\e[00m" "\e[00;33m             Privilege Escalation Script             \e[00m" "\e[00;31m#\e[00m"
@@ -46,4 +70,17 @@ echo -e "\n"
 
 echo_title "SYSTEM INFO"
 
-run_section_commands SYSTEM_INFO[@]
+run_section_commands "$(declare -p SYSTEM_INFO)"
+
+#enumerate the users and groups on the system and what they are doing
+
+echo_title "USERS AND GROUPS INFO"
+
+run_section_commands "$(declare -p USER_GROUP)"
+
+#enumerate the curent user info
+
+echo_title "CURRENT USER INFO"
+
+run_section_commands "$(declare -p CURRENT_USER)"
+
